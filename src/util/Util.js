e5ddfb5c92041f0b7669e38480e3d96bedb93e8a
Richard Rutsche <r.rutsche@code-b.com>
2016-12-09 13:10:08 +0100

    eslint util

--- src/util/Util.js
+++ src/util/Util.js
@@ -1,24 +1,24 @@
 
-export function isScrolledIntoView(element, canUseDOM) {
-	if (!canUseDOM) {
+export function isScrolledIntoView(element, isClient) {
+	if (!isClient) {
 		return false;
 	}
-	let elementTop = element.getBoundingClientRect().top,
-		elementBottom = element.getBoundingClientRect().bottom;
+	const elementTop = element.getBoundingClientRect().top;
+	const elementBottom = element.getBoundingClientRect().bottom;
 	return elementTop <= 0 && elementBottom >= 0 ||
 			elementTop >= 0 && elementBottom <= window.innerHeight ||
 			elementTop <= window.innerHeight && elementBottom >= window.innerHeight;
 }
 
-export function getWindowHeight(canUseDOM) {
-	if (!canUseDOM) {
+export function getWindowHeight(isClient) {
+	if (!isClient) {
 		return 0;
 	}
 
-	let w = window,
-		d = document,
-		e = d.documentElement,
-		g = d.getElementsByTagName('body')[0];
+	const w = window;
+	const d = document;
+	const e = d.documentElement;
+	const g = d.getElementsByTagName('body')[0];
 
 	return w.innerHeight || e.clientHeight || g.clientHeight;
 }
@@ -27,33 +27,32 @@ export function canUseDOM() {
 	return !!((typeof window !== 'undefined' && window.document && window.document.createElement));
 }
 
-export function getRelativePosition(node, canUseDOM) {
-	if (!canUseDOM) {
-		return 0;
-	}
-	let element = node;
-	let height = node.getBoundingClientRect().height;
+export function getPercentage(startpos, endpos, currentpos) {
+     const distance = endpos - startpos;
+     const displacement = currentpos - startpos;
+     return displacement / distance;
+}
+
+export function getRelativePosition(node, isClient) {
+    if (!isClient) {
+        return 0;
+    }
+    const element = node;
+    // const height = node.getBoundingClientRect().height;
     let y = Math.round(element.getBoundingClientRect().top);
 
     y = y > window.innerHeight ? window.innerHeight : y;
 
     return getPercentage(0, window.innerHeight, y);
-
 }
 
-export function getPercentage(startpos, endpos, currentpos) {
-     let distance = endpos - startpos;
-     let displacement = currentpos - startpos;
-     return displacement / distance;
-}
 
-export function setStyleProp(node, style, value, canUseDOM) {
-	if (!canUseDOM) {
-		return;
-	}
-	console.log('hello')
-	switch(style.property) {
-		case 'blur':
-			node.style.filter = 'blur(' + value + (style.unit || 'px') + ')';
-	}
+export function setStyleProp(node, style, value, isClient) {
+	// if (!canUseDOM) {
+	// 	return;
+	// }
+	// switch(style.property) {
+	// 	case 'blur':
+	// 		node.style.filter = 'blur(' + value + (style.unit || 'px') + ')';
+	// }
 }
